cf03d74ecc15063876ba903d6b2c13a67668181c
user <tre@Brian.local>
2022-07-23 13:22:12 +0300

    commit tree 66a3008de15d3a1fd7992f8197b99878f74cb129

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,16 @@
 class Author < ApplicationRecord
+    belongs_to :author
+    has_many :posts
+end
+
+class PostTag < ApplicationRecord
+    belongs_to :post
+    belongs_to :tag
+  end
+  
+  class Tag < ApplicationRecord
+    has_many :post_tags
+    has_many :posts, through: :post_tags
+  end
+
 end
